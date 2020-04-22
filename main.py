ce26a28d74a397c3a5d3404a6e39dc4cdbec83ef
rayd512 <rdizon@ualberta.ca>
2020-04-22 16:45:44 -0600

    Set test data for grid structure

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-pass
+# Using 0 to represent empty spaces, 1 to represent the starting marker, 
+# 2 to represent the end marker and 3 to represent any obstacles
+
+test_array = [[0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0],
+			  [0, 0, 0, 3, 0, 0, 0],
+			  [0, 1, 0, 3, 0, 2, 0],
+			  [0, 0, 0, 3, 0, 0, 0],
+			  [0, 0, 0, 0, 0, 0, 0]
+			  [0, 0, 0, 0, 0, 0, 0]
+			 ]
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
