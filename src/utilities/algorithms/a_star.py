a43d2a4dee0ca45c8421be171e62fc4b101b0bf2
rayd512 <rdizon@ualberta.ca>
2020-04-24 00:24:30 -0600

    WIP A star search. Do not merge

--- src/utilities/algorithms/a_star.py
+++ src/utilities/algorithms/a_star.py
@@ -1,2 +1,24 @@
+import operator
+
+class Cell():
+	
+	def __init__(self, parent, coords, grid):
+		self.parent = parent
+		self.location = coords
+		self.grid = grid
+
+		self.g = self.calculate_g()
+		self.h = self.calculate_h()
+
+	def calculate_g():
+		return tuple(map(operator.sub, self.grid.start, self.location))
+
+	def calculate_h():
+		pass
+
 def run_a_star(grid):
-	pass
+	open_list = []
+	closed_list =[]
+	open_list.append(grid.start)
+
+	while len(open_list) > 0:
